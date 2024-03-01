684a6343ef1c705023bac33fe29a1433a75fad78
dmaksimov <dmaksimov@vtb.ru>
2024-03-01 10:42:48 +0300

    put

--- src/main/java/ru/porabote/springbootrestauth/controller/FilesController.java
+++ src/main/java/ru/porabote/springbootrestauth/controller/FilesController.java
@@ -1,5 +1,6 @@
 package ru.porabote.springbootrestauth.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +14,7 @@ import java.util.Map;
 public class FilesController {
 
 
+    @Autowired
     FileService fileService;
 
 //    public FilesController() throws IOException {
