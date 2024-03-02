f78e2375fc8c9a57d96d468305582b271341cd3d
dmaksimov <dmaksimov@vtb.ru>
2024-03-02 21:04:16 +0300

    put

--- src/test/java/FileTest.java
+++ src/test/java/FileTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.*;
 import org.springframework.mock.web.MockMultipartFile;
 import ru.porabote.springbootrestauth.controller.FilesController;
+import ru.porabote.springbootrestauth.model.FileModel;
+import ru.porabote.springbootrestauth.service.FileService;
 
 import java.io.IOException;
 
@@ -24,15 +26,25 @@ public class FileTest {
     @Test
     public void testEdit() throws IOException {
 
-        FilesController controller = new FilesController();
+        FileService service = new FileService();
+
+        final String oldName = "test.txt";
+        final String newName = "testRenamed.txt";
+
+        FileModel res = service.edit(oldName, newName);
+        Assertions.assertEquals(res.getFilename(), newName);
+
+    }
+
+    @Test
+    public void testDelete() throws IOException {
+
+        FileService service = new FileService();
 
         final String fileName = "test.txt";
-        final byte[] content = "Hallo Word".getBytes();
-        MockMultipartFile mockMultipartFile =
-                new MockMultipartFile("content", fileName, "text/plain", content);
 
-        String res = controller.addFile(fileName, mockMultipartFile);
-        Assertions.assertEquals(res, "Saved");
+        boolean res = service.delete(fileName);
+        Assertions.assertTrue(res);
 
     }
 
